92e44a5490039b6aa8a6eca9d61311daac3f29a2
AshuGTx-24 <mohanty.cloudace@gmail.com>
2024-01-30 21:14:28 +0530

    strpping

--- TESTE1.py
+++ TESTE1.py
@@ -6,3 +6,7 @@ text1="ashutosh is unemployed"
 new_text=text1.replace("unemployed","focusing")
 print("new text is:", new_text)
 text2=new_text.replace("employed", "focusing")
+#strippedtext
+text3="   hi  how are you  "
+stripped_text=text3.strip()
+print("new text is:", stripped_text)
