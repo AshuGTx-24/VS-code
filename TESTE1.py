eb88d55c86956df2e1d49545daa6b4bd49ad48ad
AshuGTx-24 <mohanty.cloudace@gmail.com>
2024-01-30 21:05:16 +0530

    4th

--- TESTE1.py
+++ TESTE1.py
@@ -1,3 +1,8 @@
 text="hi how are you!!"
 length=len(text)
 print("length of text is : ", length)
+#replace text
+text1="ashutosh is unemployed"
+new_text=text1.replace("unemployed","focusing")
+print("new text is:", new_text)
+text2=new_text.replace("employed", "focusing")
