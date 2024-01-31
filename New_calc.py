0f5b26f30fc557d695acd33d305f5c104741e210
AshuGTx-24 <mohanty.cloudace@gmail.com>
2024-01-31 22:32:10 +0530

    new

---
+++ New_calc.py
@@ -0,0 +1,26 @@
+"""import Calculator as basic_calc
+
+basic_calc
+"""
+
+#percentage calculator
+"""numb1=540
+numb2=20
+percentage=numb1/100*numb2
+print(percentage)
+"""
+
+#taking user input
+"""numb3=input("enter your total number :")
+numb4=input("enter the percent you want :")
+def percentage1():
+    percent=int(numb3)/100*int(numb4)
+    print("percentage is :" , percent)
+
+percentage1()
+"""
+#way2
+def percentage2(numb5,numb6):
+    percent1=int(numb5)/100*int(numb6)
+    return percent1
+print(percentage2(input("enter your total number :"), input("enter percent :")))
