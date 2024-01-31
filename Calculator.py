0d8642f7b888fa87e35f4f26e5b7bf6543cac8c2
AshuGTx-24 <mohanty.cloudace@gmail.com>
2024-01-31 22:29:08 +0530

    day3

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,6 @@
 #normal
-num1=10
+
+"""num1=10
 num2=20
 add=num1+num2
 print("Addition is - " + str(add))
@@ -9,9 +10,10 @@ mul=num1*num2
 print("multiplication is - " + str(mul))
 div=num1/num2
 print("division is - " + str(div))
-
+"""
 #using function
-num3=input("enter your first number :")
+
+"""num3=input("enter your first number :")
 num4=input("enter your second number :")
 def add1():
     addition=int(num3)+int(num4)
@@ -30,3 +32,36 @@ add1()
 sub1()
 mul1()
 div1()
+"""
+
+"""
+this is a comment writtrn
+in more than 
+one lines
+
+you can also
+use it 
+when you want 
+to ocmment  a code
+"""
+
+"""
+calculator programm using return function
+
+"""
+def addition2(num5,num6):
+    add3=num5+num6
+    return add3
+
+def substraction2(num5,num6):
+    sub2=num5-num6
+    return sub2
+
+def multiplication2(num5,num6):
+    multi2=num5*num6
+    return multi2
+
+print(addition2(10,5))
+print(substraction2(8,5))
+print(multiplication2(7,5))
+
