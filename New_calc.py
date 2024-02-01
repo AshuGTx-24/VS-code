a5b74518eecef689dcd628f0a42d3ad320144ef8
AshuGTx-24 <mohanty.cloudace@gmail.com>
2024-02-01 18:49:53 +0530

    CLA

--- New_calc.py
+++ New_calc.py
@@ -20,7 +20,53 @@ def percentage1():
 percentage1()
 """
 #way2
+"""
 def percentage2(numb5,numb6):
     percent1=int(numb5)/100*int(numb6)
     return percent1
 print(percentage2(input("enter your total number :"), input("enter percent :")))
+"""
+
+#calculator using commnad line argumrnts
+"""""
+
+import sys
+def addition(num1,num2):
+    add=num1+num2
+    return add
+def substraction(num1,num2):
+    sub=num1-num2
+    return sub
+def multiplication(num1,num2):
+    mul=num1*num2
+    return mul
+def division(num1,num2):
+    div=num1/num2
+    return div
+num1=float(sys.argv[1])
+operation=sys.argv[2]
+num2=float(sys.argv[3])
+if operation=="add":
+    output=addition(num1,num2)
+    print(output)
+
+if operation=="sub":
+    output1=substraction(num1,num2)
+    print (output1)
+
+if operation=="mul":
+    output2=multiplication(num1,num2)
+    print(output2)
+
+if operation=="div":
+    output3=division(num1,num2)
+    print(output3)
+    """
+
+#enviroment variables
+import os
+print(os.getenv("password"))
+
+
+   
+   
